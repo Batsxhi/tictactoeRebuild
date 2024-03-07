04d083ada8e25cd3ab834d9ce46bce24258f866e
Batsxhi <wittko.sebastian@gmx.de>
2024-03-08 00:48:39 +0100

    TicTacToe Klasse hinzugefügt

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
-using namespace std 
-  
-int main() {
+#include "tictactoe.h"
 
-}
+using namespace std;
+
+enum board_status{ EMPTY = 0, PLAYER_1 = 1, PLAYER_2 = 2}
+
+string output_board_line(int spielfeld[]){
+ string output = "";
+  for(int i = 0; i < 3; i++) {
+    switch (spielfeld[i]) {
+      case EMPTY:
+        output += "  ";
+        break;
+
+      case PLAYER_1:
+        output += "X ";
+        break;
+
+      case PLAYER_2:
+        output += "O "
+
+      default:
+
+        break;
+    }
+  }
+  return output;
+} 
+
+
+class TicTacToe{
+public:
+  int spielfeld[3][3];
+
+  void print() {
+    std::cout << " " << " " << "1" << " " << "2" << " " << "3" << enld;
+    std::cout << "1" << output_board_line(spielfeld[0]) << endl;
+    std::cout << "2" << output_board_line(spielfeld[1]) << endl;
+    std::cout << "3" << output_board_line(spielfeld[2]) << endl;
+  }
+
+
+
+};
+
+
